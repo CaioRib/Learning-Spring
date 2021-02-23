2f49e67c6aef39770945f6964b94f89cb8d58eaa
caiorib <caiowsky@gmail.com>
2021-02-23 15:52:48 -0300

    Incluir novos parâmetros na entidade Aluno

--- src/main/java/project/entity/Aluno.java
+++ src/main/java/project/entity/Aluno.java
@@ -5,19 +5,30 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Builder
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"email"}))
 public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_aluno;
+    @Column(name = "id_aluno")
+    private Long id;
+    @NotNull
+    @NotEmpty
     private String nome;
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = ".+@pagseguro.com", message = "Apenas emails @pagseguro.com sao permitidos.")
+    private String email;
+    @NotNull
+    @NotEmpty
+    private String status;
 }
