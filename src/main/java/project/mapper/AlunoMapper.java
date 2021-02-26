32149e7c82ed1c22c1621dbedb6347fa18e3e551
caiorib <caiowsky@gmail.com>
2021-02-26 15:07:57 -0300

    Modificar o mapper AlunoMapper para o modelo de injecao de dependencias

--- src/main/java/project/mapper/AlunoMapper.java
+++ src/main/java/project/mapper/AlunoMapper.java
@@ -1,15 +1,15 @@
 package project.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 import project.entity.Aluno;
 import project.request.AlunoPostResquestBody;
 import project.request.AlunoPutResquestBody;
 import project.response.AlunoGetResponseBody;
 
+@Component
 @Mapper(componentModel = "spring")
 public abstract class AlunoMapper {
-    public static final AlunoMapper INSTANCE = Mappers.getMapper(AlunoMapper.class);
     public abstract Aluno toAluno(AlunoPostResquestBody alunoPostResquestBody);
     public abstract Aluno toAluno(AlunoPutResquestBody alunoPutResquestBody);
     public abstract AlunoGetResponseBody toAlunoGetResponseBody(Aluno aluno);
