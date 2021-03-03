4d58a7ee3988d0bcba158718704a40a382fb43ea
Caio Abreu de Oliveira Ribeiro <caiowsky@gmail.com>
2021-03-03 11:46:27 -0300

    Incluir novos parametros para o postResquest e getResponse bodies de Aluno

--- src/main/java/project/request/AlunoPostResquestBody.java
+++ src/main/java/project/request/AlunoPostResquestBody.java
@@ -1,9 +1,11 @@
 package project.request;
 
+import lombok.Builder;
 import lombok.Data;
 import project.entity.Turma;
 
 @Data
+@Builder
 public class AlunoPostResquestBody {
     private String nome;
     private String email;
--- src/main/java/project/response/AlunoGetResponseBody.java
+++ src/main/java/project/response/AlunoGetResponseBody.java
@@ -1,9 +1,13 @@
 package project.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import project.entity.Turma;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class AlunoGetResponseBody {
     private String nome;
     private String email;
